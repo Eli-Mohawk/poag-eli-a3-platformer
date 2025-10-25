68c137a37b4b3c6e599078ea9c90c5e5179e8a1a
Eli-Mohawk <eli.poag@mohawkcollege.ca>
2025-10-25 12:36:57 -0400

    added level class

--- poag-eli-a3-platformer/Game.cs
+++ poag-eli-a3-platformer/Game.cs
@@ -1,11 +1,9 @@
 ﻿/// TO-DO List
-/// 
-/// enter keybind in title screen
-/// 
+///
 /// finish level 2
 /// add spikes to level 2 and give them collision
 /// add win screen
-/// add life system
+/// add life system !!!
 /// add more levels
 /// add new objects and give them collision
 /// add textures
@@ -31,7 +29,7 @@ namespace MohawkGame2D
     {
         bool isGameStarted = false;
 
-        int levelTracker = 1;
+        public int levelTracker = 1;
 
         Player player = new Player();
 
@@ -40,6 +38,8 @@ namespace MohawkGame2D
         List<Spike> spikeFloor = new List<Spike>();
 
         GameOver gameOver = new GameOver();
+
+        Level levels = new Level(1);
 
         public void Setup()
         {
@@ -72,9 +72,13 @@ namespace MohawkGame2D
 
             // if for game win
 
-            levelTracker = player.gameLevel;
-            player.Update(platforms);
+            levelTracker = player.gameLevel; // tracks the players current level
+            player.Update(platforms); // adds collision
+            levels.currentLevel = levelTracker; // makes the display level = to the game level
+            platforms.Clear(); // remove all platforms
+            levels.Update(platforms); // run level code
 
+            // makes floor spikes when on level one
             if (levelTracker == 1)
             {
                 foreach (Spike spike in spikeFloor)
@@ -83,54 +87,14 @@ namespace MohawkGame2D
                 }
             }
 
+            // makes the platforms
             foreach (Platform platform in platforms)
             {
                 platform.Update();
             }
-            platforms.Clear();
-            TEMP();
+
 
             CHEAT();
-        }
-
-        void TEMP()
-        {
-            if (levelTracker == 1)
-            {
-                TEMP2();
-            }
-
-            if (levelTracker == 2)
-            {
-                TEMP3();
-            }
-        }
-
-        void TEMP2()
-        {
-            platforms.Add(new Platform(new Vector2(260, 550), new Vector2(90, 20)));
-            platforms.Add(new Platform(new Vector2(260, 550), new Vector2(90, 20)));
-            platforms.Add(new Platform(new Vector2(150, 490), new Vector2(60, 20)));
-            platforms.Add(new Platform(new Vector2(230, 420), new Vector2(35, 20)));
-            platforms.Add(new Platform(new Vector2(310, 380), new Vector2(35, 20)));
-            platforms.Add(new Platform(new Vector2(500, 540), new Vector2(40, 20)));
-            platforms.Add(new Platform(new Vector2(550, 465), new Vector2(60, 20)));
-            platforms.Add(new Platform(new Vector2(680, 420), new Vector2(40, 20)));
-            platforms.Add(new Platform(new Vector2(605, 350), new Vector2(65, 20)));
-            platforms.Add(new Platform(new Vector2(545, 300), new Vector2(30, 20)));
-            platforms.Add(new Platform(new Vector2(430, 265), new Vector2(10, 10)));
-            platforms.Add(new Platform(new Vector2(250, 280), new Vector2(50, 20)));
-            platforms.Add(new Platform(new Vector2(100, 220), new Vector2(80, 20)));
-            platforms.Add(new Platform(new Vector2(0, 155), new Vector2(50, 20)));
-            platforms.Add(new Platform(new Vector2(130, 90), new Vector2(15, 15)));
-            platforms.Add(new Platform(new Vector2(225, 80), new Vector2(10, 10)));
-            platforms.Add(new Platform(new Vector2(305, 35), new Vector2(100, 20)));
-        }
-
-        void TEMP3()
-        {
-            platforms.Add(new Platform(new Vector2(260, 550), new Vector2(90, 20)));
-            platforms.Add(new Platform(new Vector2(0, 155), new Vector2(50, 20)));
         }
 
         void DrawTitleScreen()
@@ -144,22 +108,22 @@ namespace MohawkGame2D
             };
 
             // array for x allignment
-            int controlStartX = 105; // control tab
+            int controlStartX = 45; // control tab
             int objectiveStartX = 480; // objective tab
             int[] xPosition = [controlStartX, objectiveStartX];
 
-            // title
+            // title 
             Text.Color = titleScreenColors[1];
             Text.Size = 45;
-            Text.Draw("-Ascent-", new Vector2(307, 33));
+            Text.Draw("-Ascent-", new Vector2(305.5f, 33));
             Text.Color = titleScreenColors[0];
-            Text.Draw("-Ascent-", new Vector2(304, 30));
+            Text.Draw("-Ascent-", new Vector2(302.5f, 30));
 
             // headers
             Text.Color = titleScreenColors[2];
             Text.Size = 35;
-            Text.Draw("Controls", new Vector2(xPosition[0], 104));
-            Text.Draw("Objective", new Vector2(xPosition[1], 102));
+            Text.Draw("Controls", new Vector2(xPosition[0] + 62.5f, 104));
+            Text.Draw("Objective", new Vector2(xPosition[1] + 52.5f, 102));
 
             // info
             Text.Size = 16;
@@ -170,6 +134,9 @@ namespace MohawkGame2D
             Text.Draw("- Make your way up the", new Vector2(xPosition[1], 155));
             Text.Draw("mountian to reach the long lost", new Vector2(xPosition[1], 175));
             Text.Draw("ice cream cone", new Vector2(xPosition[1], 195));
+
+            Text.Size = 25;
+            Text.Draw("Press [Enter] to start game", new Vector2(220, 401));
 
             // starts the game
             if (Input.IsKeyboardKeyPressed(KeyboardInput.Enter))
---
+++ poag-eli-a3-platformer/Level.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    public class Level
+    {
+        public int currentLevel;
+
+        public Level(int currentLevel)
+        {
+            this.currentLevel = currentLevel;
+        }
+
+        public void Setup()
+        {
+
+        }
+
+        public void Update(List<Platform> platforms)
+        {
+            Levels(platforms);
+        }
+
+        void Levels(List<Platform> platforms)
+        {
+            if (currentLevel == 1)
+            {
+                LevelOne(platforms);
+            }
+
+            if (currentLevel == 2)
+            {
+                LevelTwo(platforms);
+            }
+        }
+
+        void LevelOne(List<Platform> platforms)
+        {
+            platforms.Add(new Platform(new Vector2(260, 550), new Vector2(90, 20)));
+            platforms.Add(new Platform(new Vector2(260, 550), new Vector2(90, 20)));
+            platforms.Add(new Platform(new Vector2(150, 490), new Vector2(60, 20)));
+            platforms.Add(new Platform(new Vector2(230, 420), new Vector2(35, 20)));
+            platforms.Add(new Platform(new Vector2(310, 380), new Vector2(35, 20)));
+            platforms.Add(new Platform(new Vector2(500, 540), new Vector2(40, 20)));
+            platforms.Add(new Platform(new Vector2(550, 465), new Vector2(60, 20)));
+            platforms.Add(new Platform(new Vector2(680, 420), new Vector2(40, 20)));
+            platforms.Add(new Platform(new Vector2(605, 350), new Vector2(65, 20)));
+            platforms.Add(new Platform(new Vector2(545, 300), new Vector2(30, 20)));
+            platforms.Add(new Platform(new Vector2(430, 265), new Vector2(10, 10)));
+            platforms.Add(new Platform(new Vector2(250, 280), new Vector2(50, 20)));
+            platforms.Add(new Platform(new Vector2(100, 220), new Vector2(80, 20)));
+            platforms.Add(new Platform(new Vector2(0, 155), new Vector2(50, 20)));
+            platforms.Add(new Platform(new Vector2(130, 90), new Vector2(15, 15)));
+            platforms.Add(new Platform(new Vector2(225, 80), new Vector2(10, 10)));
+            platforms.Add(new Platform(new Vector2(305, 35), new Vector2(100, 20)));
+        }
+
+        void LevelTwo(List<Platform> platforms)
+        {
+            platforms.Add(new Platform(new Vector2(260, 550), new Vector2(90, 20)));
+            platforms.Add(new Platform(new Vector2(0, 155), new Vector2(50, 20)));
+        }
+    }
+}
